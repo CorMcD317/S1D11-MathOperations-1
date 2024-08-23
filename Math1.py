b515c98d9b1c8812d9845394c120e8a4a0572a51
Corbin McDougal <mcdoucor000@warren.k12.in.us>
2024-08-23 08:07:32 -0400

    initial commit

--- Math1.py
+++ Math1.py
@@ -1,4 +1,4 @@
 FRISBEE = 3
 COST_OF_FRISBEE = 15
-product = FRISBEE * COST_OF_FRISBEE
-print(" The product of FRISBEE and COST_OF_FRISBEE is:", product)
+Total_Cost = FRISBEE * COST_OF_FRISBEE
+print("The total cost is $", Total_Cost)
